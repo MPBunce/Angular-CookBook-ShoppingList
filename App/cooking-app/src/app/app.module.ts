22aa7b9a9f9893e7b1fb3cd2bbb97e790ca048cd
Matthew Bunce <mbunce@uoguelph.ca>
2022-08-27 16:38:51 -0400

    components

--- App/cooking-app/src/app/app.module.ts
+++ App/cooking-app/src/app/app.module.ts
@@ -4,9 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from 'src/components/Header/header.component';
+import { ShoppingEditComponent } from 'src/components/shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from 'src/components/shopping-list/shopping-list.component';
+import { RecipesComponent } from 'src/components/recipes/recipes.component';
+import { RecipeListComponent } from 'src/components/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from 'src/components/recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from 'src/components/recipes/recipe-detail/recipe-detail.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingEditComponent,
+    ShoppingListComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
