12258f70f7a5d00f06fe47b3aa4ba3572b351495
Matthew Bunce <mbunce@uoguelph.ca>
2022-08-28 16:27:42 -0400

    on section 9

--- App/cooking-app/src/components/recipes/recipe-list/recipe-list.component.ts
+++ App/cooking-app/src/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  recipes: Recipe[] = [
+
+    new Recipe(
+      'A test recipe',
+      'This is a test recipie',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2022%2F07%2F20%2FSkillet-Lemon-Pepper-Salmon-1x1-1.jpg&w=400&h=400&c=sc&poi=%5B1160%2C920%5D&q=60&rect=320%2C144%2C1966%2C1789'
+    ),
+    new Recipe(
+      'Another test recipe',
+      'This is another test recipie',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2022%2F07%2F20%2FSkillet-Lemon-Pepper-Salmon-1x1-1.jpg&w=400&h=400&c=sc&poi=%5B1160%2C920%5D&q=60&rect=320%2C144%2C1966%2C1789'
+    )
+
+  ];
+
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
--- App/cooking-app/src/components/shopping-list/shopping-list.component.ts
+++ App/cooking-app/src/components/shopping-list/shopping-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
+
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 12),
+    new Ingredient('Oranges', 6)
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onIngredientAdded(ingredient){
+    this.ingredients.push(ingredient)
   }
 
 }
